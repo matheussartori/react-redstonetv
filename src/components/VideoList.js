1dac544085871a63cc7e8a740338c09fc9770ed6
Matheus Sartori <ms.sartori@outlook.com.br>
2020-08-08 12:44:40 -0300

    VideoList structure

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { List, Image } from 'semantic-ui-react'
 
-const VideoList = props => {
+class VideoList extends Component {
+  render() {
     return (
-        <div className='video-list'>
-            <h1>Video List</h1>
-        </div>
+      <div className='video-list'>
+        <List animated verticalAlign='middle'>
+          <List.Item>
+            <Image src='' avatar />
+            <List.Content>
+              <List.Header>Título</List.Header>
+            </List.Content>
+          </List.Item>
+        </List>
+
+        <List animated verticalAlign='middle'>
+          <List.Item>
+            <Image src='' avatar />
+            <List.Content>
+              <List.Header>Título</List.Header>
+            </List.Content>
+          </List.Item>
+        </List>
+
+        <List animated verticalAlign='middle'>
+          <List.Item>
+            <Image src='' avatar />
+            <List.Content>
+              <List.Header>Título</List.Header>
+            </List.Content>
+          </List.Item>
+        </List>
+      </div>
     )
+  }
 }
 
 export default VideoList
