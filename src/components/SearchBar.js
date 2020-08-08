0fb16e3b890dbbba2b2a190a2c9896868827612a
Matheus Sartori <ms.sartori@outlook.com.br>
2020-08-08 12:15:44 -0300

    mapDispatchToProps in SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Segment, Input } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+
+import { searchVideo } from '../store/actions/searchVideo';
 
 class SearchBar extends Component {
   searchTerm = e => {
     if (e.keyCode === 13) {
       const term = e.target.value;
-      console.log(term);
+      this.props.searchVideo(term);
     }
   }
 
@@ -24,4 +27,10 @@ class SearchBar extends Component {
   }
 }
 
-export default SearchBar
+const mapDispatchToProps = dispatch => {
+  return {
+    searchVideo: term => dispatch(searchVideo(term))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar)
