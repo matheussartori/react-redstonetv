f9c1fe880698ae8f7a2611aaca13b495ef3567a6
Matheus Sartori <ms.sartori@outlook.com.br>
2020-08-08 22:26:33 -0300

    Videos from api query, thumbnail and title

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,39 +1,39 @@
 import React, { Component } from 'react';
 import { List, Image } from 'semantic-ui-react'
+import { connect } from 'react-redux';
 
 class VideoList extends Component {
+
+  renderVideo = video => {
+    return (
+      <List animated verticalAlign='middle'>
+        <List.Item>
+          <Image src={video.snippet.thumbnails.default.url} />
+          <List.Content>
+            <List.Header>{video.snippet.title}</List.Header>
+          </List.Content>
+        </List.Item>
+      </List>
+    )
+  }
+
   render() {
     return (
       <div className='video-list'>
-        <List animated verticalAlign='middle'>
-          <List.Item>
-            <Image src='' avatar />
-            <List.Content>
-              <List.Header>Título</List.Header>
-            </List.Content>
-          </List.Item>
-        </List>
-
-        <List animated verticalAlign='middle'>
-          <List.Item>
-            <Image src='' avatar />
-            <List.Content>
-              <List.Header>Título</List.Header>
-            </List.Content>
-          </List.Item>
-        </List>
-
-        <List animated verticalAlign='middle'>
-          <List.Item>
-            <Image src='' avatar />
-            <List.Content>
-              <List.Header>Título</List.Header>
-            </List.Content>
-          </List.Item>
-        </List>
+        {
+          this.props.videos.map(video => this.renderVideo(video))
+        }
       </div>
     )
   }
 }
 
-export default VideoList
+const mapStateToProps = state => {
+  return {
+    videos: state.searchVideoReducer.videos,
+    isFetching: state.searchVideoReducer.isFetching,
+    error: state.searchVideoReducer.error
+  }
+}
+
+export default connect(mapStateToProps)(VideoList)
